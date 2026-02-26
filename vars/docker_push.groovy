1041bcfaf72bd26a646a4677c35b935bbade4b6b
Siddharth <123649860+sid0109@users.noreply.github.com>
2026-02-26 13:38:44 +0530

    Add docker_push.groovy for Docker image deployment

---
+++ vars/docker_push.groovy
@@ -0,0 +1,10 @@
+def call(String Project, String ImageTag, String dockerhubuser){
+                echo "Pushing code to docker hub"
+                withCredentials([usernamePassword('credentialsId':"dockerHubCred", 
+                passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
+                sh "docker push ${env.dockerHubUser}/notes-app:latest"
+                }
+}
+  
